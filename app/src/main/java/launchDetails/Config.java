0a32ae7ff67a6c1bc6bacbcb590869944f21556a
light <nidhia3804@gmail.com>
2016-11-30 20:38:25 +0530

    Sample_one

--- app/src/main/java/launchDetails/Config.java
+++ app/src/main/java/launchDetails/Config.java
@@ -26,4 +26,5 @@ public class Config {
     public static final String rsvp_phone_one="rsvp_phone_one";
     public static final String rsvp_phone_two="rsvp_phone_two";
     public static final String rsvp_phone="rsvp_phone_two";
+    public static final String rsvpSample="rsvp_phone_two";
 }
